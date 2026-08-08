85282f1156d9562e3a7264c15d98f0e692b514d7
prateekv27 <prateekv887@gmail.com>
2026-08-08 09:02:42 +0530

    min in rotated array

--- binary_search/peakmountainarray.cpp
+++ binary_search/peakmountainarray.cpp
@@ -1,18 +1,27 @@
-//largest 
-int main(){
-    int sz = 5;
-                LARGEST
-    int arr[sz];
-    for(int i =0;i<sz;i++){
-        cin>>arr[i];
-    }
-    int largest = arr[0];
-    for(int i =0;i<sz;i++){
-        if(arr[i]>largest){
-            largest=arr[i];
-           
+//peakindexmountaiarray
+//leetcode ques no 852
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start = 0;
+        int end = arr.size()-1;
+        int mid;
+        int ans;
+        while(start<=end){
+            mid = end + (start - end)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                ans = mid;
+                break;
+            }
+            else if(arr[mid]<arr[mid-1]){
+                end = mid -1;
+            }
+            else{
+                start = mid +1;
+            }
         }
-
+        return ans;
+        
     }
-
-
+};
