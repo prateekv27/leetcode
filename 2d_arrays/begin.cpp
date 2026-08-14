3f0c52da51c001084e9308bafb7378e88c810dfc
prateekv27 <prateekv887@gmail.com>
2026-08-14 09:31:06 +0530

    add 2 matrix

--- 2d_arrays/begin.cpp
+++ 2d_arrays/begin.cpp
@@ -6,16 +6,38 @@ void printcol(int arr[][4] , int row, int col){
     cout<<arr[i][j];
        
 }
-
-        //search element
+            //add 2 matrix
 int main(){
-    int arr[5][4] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-    int x = 11;
+    int arr1[5][4] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int arr2[5][4] = {21,22,23,24,25,26,27,28,29,210,211,212,213,14,15,16,17,18,19,20};
+    int arr3[5][4];
     for(int i =0;i<5;i++)
     for(int j =0;j<4;j++)
-    if(arr[i][j]==x){
-        cout<<"yess";
-        return 0;
-    }
-    cout<<"no";
+    arr3[i][j] = arr1[i][j] + arr2[i][j];
+
+    for(int i =0;i<5;i++)
+    for(int j =0;j<4;j++)
+    cout<<arr3[i][j]<<endl;
+
 }
+
+
+
+
+
+
+
+
+        //search element
+// int main(){
+//     int arr[5][4] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+//     int x = 11;
+//     for(int i =0;i<5;i++)
+//     for(int j =0;j<4;j++)
+//     if(arr[i][j]==x){
+//         cout<<"yess";
+//         return 0;
+//     }
+//     cout<<"no";
+// }
+
