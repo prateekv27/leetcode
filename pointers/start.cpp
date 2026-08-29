bb026e38dff9fc9138742bd78267ab73ad4b6439
prateekv27 <prateekv887@gmail.com>
2026-08-29 11:26:46 +0530

    pointer by refernce

--- pointers/start.cpp
+++ pointers/start.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 using namespace std;
 
+// int main(){
+//     int a = 10;
+//     int* ptr = &a;
+//     cout<<ptr<<endl;
+    
+//     //derefrencing
+//     cout<<*(ptr)<<endl;
+
+//     int** q = &ptr;
+//     cout<< *(q)<<endl;
+//     cout<<**(q)<<endl;
+
+
+
+// }
+
+//PASS BY REFERNECE
+void changeA(int* ptr){
+    *ptr = 20;//derefrencing to change
+}
+
 int main(){
     int a = 10;
-    int* ptr = &a;
-    cout<<ptr<<endl;
+    changeA(&a);
+    cout<<a<<endl;
 }
