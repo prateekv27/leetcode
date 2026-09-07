e34951d2456ad8a83270f634a57fbe1d3a1a7d03
prateekv27 <prateekv887@gmail.com>
2026-09-07 22:37:14 +0530

    dyanmic array allocation of1d and 2d arrays

--- pointers/dynamicarray.cpp
+++ pointers/dynamicarray.cpp
@@ -1,26 +1,57 @@
 #include <iostream>
 using namespace std;
 
-int getsum(int *arr , int n){
-    int sum = 0;
-    for (int i =0;i<n;i++){
-        sum+=arr[i];
+// int getsum(int *arr , int n){
+//     int sum = 0;
+//     for (int i =0;i<n;i++){
+//         sum+=arr[i];
 
 
-    }
-    return sum;
-}
+//     }
+//     return sum;
+// }
 
-int main(){
-    int n;
-    cin>>n;
+// int main(){
+//     int n;
+//     cin>>n;
     
-    int* arr = new int[n];
-    for(int i =0;i<n;i++){
-        cin>>arr[i];
+//     int* arr = new int[n];
+//     for(int i =0;i<n;i++){
+//         cin>>arr[i];
+//     }
+
+//     int ans = getsum(arr , n);
+//     cout<<ans<<endl;
+
+// }
+
+
+
+                            //2d array
+int main(){
+    int row;
+    cin>>row;
+    int col;
+    cin>>col;
+    int** arr = new int*[row];
+    for(int i =0;i<row;i++){
+        arr[i] = new int[col];
     }
 
-    int ans = getsum(arr , n);
-    cout<<ans<<endl;
+    for(int i = 0;i<row;i++){
+        for(int j = 0;j<col;j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+    for(int i = 0;i<row;i++){
+        for(int j =0;j<col;j++){
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
+    }
 
+    for(int i =0;i<row;i++){
+        delete [] arr[i];
+    }
+    delete []arr;
 }
