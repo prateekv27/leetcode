a5811e7b114c4971ade1b3afeb80ab5e286cbc13
prateekv27 <prateekv887@gmail.com>
2026-09-08 22:50:26 +0530

    macros and its types

--- extra/keywords.cpp
+++ extra/keywords.cpp
@@ -2,9 +2,38 @@
 #include <iostream>
 using namespace std;
 
-#define PI 3.14
+// #define PI 3.14
 
+// int main(){
+//     int area = PI *8*8;
+//     cout<<area;
+// }
+
+//1 Object-like Macro
+
+#define MAX 100
 int main(){
-    int area = PI *8*8;
-    cout<<area;
+
+    int arr[MAX];
+}
+
+//2Function-like Macro
+
+#define SQUARE(x) (x * x)
+int main(){
+    int a = SQUARE(7);
+}
+
+//undef 
+#define X 10
+int main(){
+
+cout << X;   // 10
+
+#undef X
+
+#define X 20
+
+cout << X;   // 20
+
 }
