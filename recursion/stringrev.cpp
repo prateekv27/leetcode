2b2f57368402aa6ae1a65120c280255994e35db9
prateekv27 <prateekv887@gmail.com>
2026-09-15 01:38:37 +0530

    palindrom string

--- recursion/stringrev.cpp
+++ recursion/stringrev.cpp
@@ -1,21 +1,54 @@
 #include <iostream>
 using namespace std;
 
-void rev(int i , string &s){
-    if(i>s.length()/2){
-        return ;
+// void rev(int i , string &s){
+//     if(i>s.length()/2){
+//         return ;
+//     }
+
+//     swap(s[i],s[s.length()-i-1]);
+//     rev(i+1,s);
+
+
+// }
+
+// int main(){
+//     string r = "abcdef";
+//     rev(0,r);
+//     cout<<r;
+
+    
+// }
+
+
+//check palindrome
+
+
+bool checkpal(string s , int i , int j){
+    if (i>j){
+        return true;
     }
 
-    swap(s[i],s[s.length()-i-1]);
-    rev(i+1,s);
+    if(s[i]!=s[j]){
+        return false;
+    }
 
+    return checkpal(s , i+1, j-1);
+    
 
 }
 
 int main(){
-    string r = "abcdef";
-    rev(0,r);
-    cout<<r;
+    string r = "racecar";
+    checkpal(r , 0 , r.length()-1);
+    if(checkpal){
+        cout<<true;
+    }
+    else{
+        cout<<false;
+    }
+    
 
     
 }
+
